f5537c8cfa4345d9e3521378a4545b334afff790
Quang Duy <justinquangduy@gmail.com>
2022-09-06 11:08:55 +0700

    Action Return Types and Status Code

---
+++ MyWebApiApp/MyWebApiApp/Controllers/LoaisController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Data;
+using MyWebApiApp.Models;
+using System.Linq;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var dsLoai = _context.Loais.ToList();
+                return Ok(dsLoai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAll(int id)
+        {
+            var dsLoai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+            if (dsLoai != null)
+            {
+                return Ok(dsLoai);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CreateNew(LoaiModel loaiModel)
+        {
+            try
+            {
+                var loai = new Loai
+                {
+                    TenLoai = loaiModel.TenLoai
+                };
+                _context.Add(loai);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created,loai);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult GetAllById(int id,LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+            if (loai != null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLoaiById(int id, LoaiModel model)
+        {
+            var loai = _context.Loais.SingleOrDefault(l => l.MaLoai == id);
+            if (loai != null)
+            {
+                _context.Remove(loai);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Data/HangHoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebApiApp.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TenHh { get; set; }
+
+        public string Mota { get; set; }
+
+        [Range(0,double.MaxValue)]
+        public double DonGia { get; set; }
+
+        public byte GiamGia { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+
+        public ICollection<DonHangChiTiet> DonHangChiTiets { get; set; }
+        public HangHoa()
+        {
+            DonHangChiTiets = new HashSet<DonHangChiTiet>();
+        }
+    }
+}
